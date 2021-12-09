1d58d9cc35368c228c9b0364186f43e8cfa71cc9
Raphael <raphacabangbang@gmail.com>
2021-12-09 10:56:20 +0000

    added all buttons

--- Lab 5/lecture6_final/app.js
+++ Lab 5/lecture6_final/app.js
@@ -1,25 +1,145 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
-app.listen(3000, function() {
-    console.log('Server running on port 3000');
+app.listen(3000, function () {
+  console.log("Server running on port 3000");
 });
 
-app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/index.html");
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/sum", function(req, res) {
-    const num1 = parseFloat(req.body.num1);
-    const num2 = parseFloat(req.body.num2);
-    console.log(req.body);
-    // const result = num1 + num2;
-    console.log(num1 + num2);
-    const result = {
-        result: num1 + num2
-    };
-    res.json(result);
+app.post("/sum", function (req, res) {
+  const num1 = parseFloat(req.body.num1);
+  const num2 = parseFloat(req.body.num2);
+  console.log(req.body);
+  // const result = num1 + num2;
+  console.log(num1 + num2);
+  const result = {
+    result: num1 + num2,
+  };
+  res.json(result);
+});
+
+app.post("/sub", function (req, res) {
+  const num1 = parseFloat(req.body.num1);
+  const num2 = parseFloat(req.body.num2);
+  console.log(req.body);
+  // const result = num1 + num2;
+  console.log(num1 - num2);
+  const result = {
+    result: num1 - num2,
+  };
+  res.json(result);
+});
+
+app.post("/multi", function (req, res) {
+  const num1 = parseFloat(req.body.num1);
+  const num2 = parseFloat(req.body.num2);
+  console.log(req.body);
+  // const result = num1 + num2;
+  console.log(num1 * num2);
+  const result = {
+    result: num1 * num2,
+  };
+  res.json(result);
+});
+
+app.post("/div", function (req, res) {
+  const num1 = parseFloat(req.body.num1);
+  const num2 = parseFloat(req.body.num2);
+  console.log(req.body);
+  // const result = num1 + num2;
+  console.log(num1 / num2);
+  const result = {
+    result: num1 / num2,
+  };
+  res.json(result);
+});
+
+app.post("/mod", function (req, res) {
+  const num1 = parseFloat(req.body.num1);
+  const num2 = parseFloat(req.body.num2);
+  console.log(req.body);
+  // const result = num1 + num2;
+  console.log(num1 % num2);
+  const result = {
+    result: num1 % num2,
+  };
+  res.json(result);
+});
+
+app.post("/exp", function (req, res) {
+  const num1 = parseFloat(req.body.num1);
+  const num2 = parseFloat(req.body.num2);
+  console.log(req.body);
+  // const result = num1 + num2;
+  console.log(num1 ** num2);
+  const result = {
+    result: num1 ** num2,
+  };
+  res.json(result);
+});
+
+app.post("/log", function (req, res) {
+  const num1 = parseFloat(req.body.num1);
+  const num2 = parseFloat(req.body.num2);
+  console.log(req.body);
+  // const result = num1 + num2;
+  console.log(Math.log(num2) / Math.log(num1));
+  const result = {
+    result: Math.log(num2) / Math.log(num1),
+  };
+  res.json(result);
+});
+
+app.post("/root", function (req, res) {
+  const num1 = parseFloat(req.body.num1);
+  const num2 = parseFloat(req.body.num2);
+  console.log(req.body);
+  // const result = num1 + num2;
+  console.log(Math.pow(num1, 1 / num2));
+  const result = {
+    result: Math.pow(num1, 1 / num2),
+  };
+  res.json(result);
+});
+
+app.post("/sin", function (req, res) {
+  const num1 = parseFloat(req.body.num1);
+  const num2 = parseFloat(req.body.num2);
+  console.log(req.body);
+  // const result = num1 + num2;
+  console.log(Math.sin(num1));
+  const result = {
+    result: Math.sin(num1),
+  };
+  res.json(result);
+});
+
+app.post("/cosine", function (req, res) {
+  const num1 = parseFloat(req.body.num1);
+  const num2 = parseFloat(req.body.num2);
+  console.log(req.body);
+  // const result = num1 + num2;
+  console.log(Math.cos(num1));
+  const result = {
+    result: Math.cos(num1),
+  };
+  res.json(result);
+});
+
+app.post("/tangent", function (req, res) {
+  const num1 = parseFloat(req.body.num1);
+  const num2 = parseFloat(req.body.num2);
+  console.log(req.body);
+  // const result = num1 + num2;
+  console.log(Math.tan(num1));
+  const result = {
+    result: Math.tan(num1),
+  };
+  res.json(result);
 });
--- Lab 5/lecture6_final/public/index.js
+++ Lab 5/lecture6_final/public/index.js
@@ -1,20 +1,139 @@
 $("#btnSum").click(function () {
-    const data = {
-        num1: $("#num1")[0].value,
-        num2: $("#num2")[0].value,
-    };
-    console.log(data);
-    const post = $.post("http://localhost:3000/sum", data);
-    // post.fail();
-    post.done(handleDone);
-    post.fail(handleFail);
+  const data = {
+    num1: $("#num1")[0].value,
+    num2: $("#num2")[0].value,
+  };
+  console.log(data);
+  const post = $.post("http://localhost:3000/sum", data);
+  // post.fail();
+  post.done(handleDone);
+  post.fail(handleFail);
+});
+
+$("#btnSub").click(function () {
+  const data = {
+    num1: $("#num1")[0].value,
+    num2: $("#num2")[0].value,
+  };
+  console.log(data);
+  const post = $.post("http://localhost:3000/sub", data);
+  // post.fail();
+  post.done(handleDone);
+  post.fail(handleFail);
+});
+
+$("#btnMulti").click(function () {
+  const data = {
+    num1: $("#num1")[0].value,
+    num2: $("#num2")[0].value,
+  };
+  console.log(data);
+  const post = $.post("http://localhost:3000/multi", data);
+  // post.fail();
+  post.done(handleDone);
+  post.fail(handleFail);
+});
+
+$("#btnDiv").click(function () {
+  const data = {
+    num1: $("#num1")[0].value,
+    num2: $("#num2")[0].value,
+  };
+  console.log(data);
+  const post = $.post("http://localhost:3000/div", data);
+  // post.fail();
+  post.done(handleDone);
+  post.fail(handleFail);
+});
+
+$("#btnMod").click(function () {
+  const data = {
+    num1: $("#num1")[0].value,
+    num2: $("#num2")[0].value,
+  };
+  console.log(data);
+  const post = $.post("http://localhost:3000/mod", data);
+  // post.fail();
+  post.done(handleDone);
+  post.fail(handleFail);
+});
+
+$("#btnExp").click(function () {
+  const data = {
+    num1: $("#num1")[0].value,
+    num2: $("#num2")[0].value,
+  };
+  console.log(data);
+  const post = $.post("http://localhost:3000/exp", data);
+  // post.fail();
+  post.done(handleDone);
+  post.fail(handleFail);
+});
+
+$("#btnLog").click(function () {
+  const data = {
+    num1: $("#num1")[0].value,
+    num2: $("#num2")[0].value,
+  };
+  console.log(data);
+  const post = $.post("http://localhost:3000/log", data);
+  // post.fail();
+  post.done(handleDone);
+  post.fail(handleFail);
+});
+
+$("#btnRoot").click(function () {
+  const data = {
+    num1: $("#num1")[0].value,
+    num2: $("#num2")[0].value,
+  };
+  console.log(data);
+  const post = $.post("http://localhost:3000/root", data);
+  // post.fail();
+  post.done(handleDone);
+  post.fail(handleFail);
+});
+
+$("#btnSin").click(function () {
+  const data = {
+    num1: $("#num1")[0].value,
+    num2: $("#num2")[0].value,
+  };
+  console.log(data);
+  const post = $.post("http://localhost:3000/sin", data);
+  // post.fail();
+  post.done(handleDone);
+  post.fail(handleFail);
+});
+$("#btnCos").click(function () {
+  const data = {
+    num1: $("#num1")[0].value,
+    num2: $("#num2")[0].value,
+  };
+  console.log(data);
+  const post = $.post("http://localhost:3000/cosine", data);
+  // post.fail();
+  post.done(handleDone);
+  post.fail(handleFail);
+});
+
+$("#btnTan").click(function () {
+  const data = {
+    num1: $("#num1")[0].value,
+    num2: $("#num2")[0].value,
+  };
+  console.log(data);
+  const post = $.post("http://localhost:3000/tangent", data);
+  // post.fail();
+  post.done(handleDone);
+  post.fail(handleFail);
 });
 
 function handleDone(response, status, xhr) {
-    const result = response.result;
-    $(`<div>${result}</div><a href='/'>Clear</a>`).insertAfter("#mainForm");
+  const result = response.result;
+  $(`<div>${result}</div><a href='/'>Clear</a>`).insertAfter("#mainForm");
 }
 
 function handleFail(response, status, xhr) {
-    $(`<div>Error!</div><a href='/'>Clear</a>`).insertAfter("#mainForm");
+  $(`<div>Error!</div><a href='/'>Clear</a>`).insertAfter("#mainForm");
 }
